2736ea131d3b75a8b8e72634d596f7483ff41fbd
jhonjlinaresb <jhonjlinaresb@gmail.com>
2020-10-14 22:10:09 +0200

    Changes in OrderController.js
    Bugs in the Split of auth.js

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,29 +1,25 @@
 const {
     Order,
-    Movie,
-    User
+    User,
+    Movie
 } = require('../models');
 
-
 const OrderController = {
-
     async getAll(req, res) {
         try {
-            const orders = await Order.findAll({
+            const orders = await Order.findAll ({
                 attributes: {
                     exclude: ['UserId']
                 },
-                include: [{
+                include:[{
                     model: Movie,
-                    attributes: ['title', 'poster_path'],
-                    through: {
+                    attributes: ['title'],
+                    through:{
                         attributes: []
                     }
-                }, {
+                },{
                     model: User,
-                    attributes: {
-                        exclude: ['password']
-                    }
+                    attributes: ['email']
                 }]
             });
             res.send(orders);
@@ -35,27 +31,28 @@ const OrderController = {
             })
         }
     },
-    create(req, res) {
-        const returnDate = new Date();
-        returnDate.setDate(returnDate.getDate() + 2)
-        Order.create({
-                status: 'cancelled',
+    async create(req,res){
+        try {
+            const returnDate = new Date();
+            returnDate.setDate(returnDate.getDate() + 3)
+            const order = await Order.create({
+                status: 'Alquilada',
                 returnDate,
                 UserId: req.user.id
+                });
+                await order.addMovie(req.body.Movies);
+                res.send ({
+                    message: 'Added successfully in your order'
+                }); 
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                error,
+                message: 'There was a problem trying to get the orders'
             })
-            .then(order => {
-                return order.addMovie(req.body.movies); //añade en OrderMovies las movies con el OrderId
-            })
-            .then(() => res.send({
-                message: 'Order successfully created!'
-            }))
-            .catch(error => {
-                console.error(error);
-                res.status(500).send({
-                    message: 'There was a problema trying to create the order'
-                })
-            })
+        }
     }
+
 }
 
-module.exports = OrderController
+module.exports = OrderController;
